23b33b3e8f45c4d15b1bce9c608035f92c2299f3
Masoud - Home <masoud.brilliant@hotmail.com>
2023-02-25 00:59:27 +0330

    Working with lifecycles

---
+++ HotelManagement.Server/Models/BlazorAmenities.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelManagement.Server.Models;
+
+public class BlazorAmenities
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+}
