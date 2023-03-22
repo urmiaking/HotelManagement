a4aaaa85038051f998048c83e63df3f3a946b0d3
Masoud - Home <masoud.brilliant@hotmail.com>
2023-03-22 20:51:48 +0330

    Add Api project

---
+++ HotelManagement.Api/Program.cs
@@ -0,0 +1,35 @@
+using HotelManagement.Business.Repositories.Implementations;
+using HotelManagement.Business.Repositories.Interfaces;
+using HotelManagement.DataAccess.Data;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IHotelRoomRepository, HotelRoomRepository>();
+builder.Services.AddScoped<IHotelImagesRepository, HotelImageRepository>();
+builder.Services.AddScoped<IHotelAmenityRepository, HotelAmenityRepository>();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
