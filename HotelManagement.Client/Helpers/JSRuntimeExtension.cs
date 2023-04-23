4bda11ffe0424da56cad3f6590ad29814a9fd0d1
Masoud - Home <masoud.brilliant@hotmail.com>
2023-04-23 16:51:29 +0330

    Add Toastr in client app

---
+++ HotelManagement.Client/Helpers/JSRuntimeExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace HotelManagement.Client.Helpers;
+
+public static class JsRuntimeExtension
+{
+    public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+    }
+
+    public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+    }
+}
---
+++ HotelManagement.Client/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type == "success") {
+        toastr.success(message, 'عملیات موفقیت آمیز بود!')
+    }
+    else if (type == "error") {
+        toastr.error(message, 'عملیات با خطا مواجه شد!')
+    }
+}
