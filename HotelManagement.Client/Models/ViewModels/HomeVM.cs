1303e88775846932c0ecf3864c29a7dfb3a89006
Masoud - Home <masoud.brilliant@hotmail.com>
2023-05-15 00:33:13 +0330

    Add index page for client

---
+++ HotelManagement.Client/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelManagement.Client.Models.ViewModels;
+
+public class HomeVm
+{
+    public DateTime StartDate { get; set; } = DateTime.Now;
+    public DateTime EndDate { get; set; }
+    public int NumberOfNights { get; set; } = 1;
+}
