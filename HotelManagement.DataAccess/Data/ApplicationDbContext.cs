d97fd75b4243089b92664d39e091339051242088
Masoud - Home <masoud.brilliant@hotmail.com>
2023-06-30 12:03:31 +0330

    Change identityUser

--- HotelManagement.DataAccess/Data/ApplicationDbContext.cs
+++ HotelManagement.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using HotelManagement.DataAccess.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelManagement.DataAccess.Data;
 
-public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     { }
@@ -12,4 +13,5 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public required DbSet<HotelRoom> HotelRooms { get; set; }
     public required DbSet<HotelRoomImage> HotelRoomsImage { get; set; }
     public required DbSet<HotelAmenity> HotelAmenities { get; set; }
+    public required DbSet<ApplicationUser> ApplicationUser { get; set; }
 }
